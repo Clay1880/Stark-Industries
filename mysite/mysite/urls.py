d706464631f0ef4da10c4bc53a9048fa831139c6
Prince Singh <96003886+Clay1880@users.noreply.github.com>
2022-02-01 16:19:39 +0530

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = "Stark Industries"
 admin.site.site_title = "Stark Industries Admin Portal"
@@ -24,4 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
     
-]
+]+static(settings.STATIC_URL , document.root=settings.STATIC_ROOT)
